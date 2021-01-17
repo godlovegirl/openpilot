149745f85914844138b3b25a08ca9aef54933d11
Casey Francis <@CFranHonda>
2021-01-16 20:45:51 -0800

    Revert "Update ui.hpp"
    This reverts commit 4504f867097930b2fe34ef4fdf1611e372e2fbf2.

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -120,8 +120,6 @@ typedef struct UIScene {
   float alert_blinking_rate;
   cereal::ControlsState::AlertSize alert_size;
 
-  bool recording;
-
   cereal::HealthData::HwType hwType;
   int satelliteCount;
   NetStatus athenaStatus;
