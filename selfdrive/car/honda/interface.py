c6c09a6f99cf88b45b45e7e1cc8216f57f5b62f6
Casey Francis <@CFranHonda>
2021-03-20 21:02:01 -0700

    chill out gasmax

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -448,7 +448,7 @@ class CarInterface(CarInterfaceBase):
 
     if candidate in HONDA_BOSCH:
       ret.gasMaxBP = [0.0, 5., 10., 22., 35.] # m/s #0, 11, 22, 49, 78 mph
-      ret.gasMaxV = [0.35, 0.25, 0.20, 0.17, 0.15] #lessen gasMax as speed increases
+      ret.gasMaxV = [0.3, 0.22, 0.18, 0.15, 0.13] #lessen gasMax as speed increases
       ret.brakeMaxBP = [0.]  # m/s
       ret.brakeMaxV = [1.]   # max brake allowed
     else:
